c20637d4bf3162876600b9ee8af1e904ee93504c
Philipp <philipp@ouun.io>
2020-05-05 17:17:38 +0200

    Minor JS fix

--- public/js/repack-public.js
+++ public/js/repack-public.js
@@ -7,29 +7,36 @@
 			const $span = '<span class="celebrate-repack"></span>';
 			$( 'input[name="shipping_repack"]' ).parent().append( $span );
 
+			//  Initially set coupon
+			apply_repack_coupon_code();
+
 			// Update cart if checkbox changes
 			$( "form.checkout" ).on(
-				"load change",
+				"change",
 				"input#shipping_repack",
 				function() {
-					let data = {
-						action: 'update_order_review',
-						security: wc_checkout_params.update_order_review_nonce,
-						nonce_token: repack.security,
-						post_data: $( 'form.checkout' ).serialize()
-					};
-
-					$.post(
-						repack.ajax_url,
-						data,
-						function()
-						{
-							$( 'body' ).trigger( 'update_checkout' );
-						}
-					);
+					apply_repack_coupon_code();
 				}
 			);
 		}
 	);
+
+	function apply_repack_coupon_code () {
+		let data = {
+			action: 'update_order_review',
+			security: wc_checkout_params.update_order_review_nonce,
+			nonce_token: repack.security,
+			post_data: $( 'form.checkout' ).serialize()
+		};
+
+		$.post(
+			repack.ajax_url,
+			data,
+			function()
+			{
+				$( 'body' ).trigger( 'update_checkout' );
+			}
+		);
+	}
 
 })( jQuery );
