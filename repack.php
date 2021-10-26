275dfdc3398d04ed5954ad2f7528501ee03a5f9f
Philipp <philipp@ouun.io>
2021-10-26 19:44:04 +0200

    WC tested up to: 5.8

--- repack.php
+++ repack.php
@@ -18,7 +18,7 @@
  * Domain Path:       /languages
  *
  * WC requires at least: 3.6
- * WC tested up to: 5.3
+ * WC tested up to: 5.8
  *
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
